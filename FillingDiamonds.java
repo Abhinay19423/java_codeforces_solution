7b7cbd9930d9cbbb0e9a5f38d9d8592cc925e140
Abhinay19423 <148855595+Abhinay19423@users.noreply.github.com>
2025-05-28 19:11:48 +0530

    1339A - Filling Diamonds solved

---
+++ FillingDiamonds.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class FillingDiamonds {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+//		int[] dp = new int[]
+		
+		// there are exactly n ways to fill 
+		
+		while(t-- > 0) {
+			System.out.println(sc.nextInt());
+		}
+
+	}
+
+}
