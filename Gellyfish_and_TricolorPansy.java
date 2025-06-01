ef1ad9008d596a6441b9ed635f38d3ef01f4cc2f
Abhinay19423 <148855595+Abhinay19423@users.noreply.github.com>
2025-06-01 12:13:46 +0530

    A - Gellyfish ans Tricolor pansy

---
+++ Gellyfish_and_TricolorPansy.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Gellyfish_and_TricolorPansy {
+	private static Scanner sc = new Scanner(System.in);
+	
+	private static void solve() {
+		long a = sc.nextLong(), b = sc.nextLong(), c = sc.nextLong(), d = sc.nextLong();
+		
+		long miniG = Math.min(a, c), miniF = Math.min(b, d);
+		
+		if(miniG < miniF)
+			System.out.println("Flower");
+		else
+			System.out.println( "Gellyfish");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int t = sc.nextInt();
+		for(int i =0; i< t; i++)
+			solve();
+		sc.close();
+	}
+
+}
